903d83a26178848a559a98f1dc03182f70c86429
tbintang889 <115480259+tbintang889@users.noreply.github.com>
2024-09-26 02:24:35 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::middleware('role:admin')->group(function () {
+            Route:: get('/admin', function()   {
+                echo 1;
+            })->name('admin');
+
+        });
 });
 
 require __DIR__.'/auth.php';
