dba236c9acf9a141a9173b2cda95c81172ca1cdd
tbintang889 <115480259+tbintang889@users.noreply.github.com>
2024-09-29 01:24:36 +0700

    route web w module

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,38 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Modules\Profile\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('admin.index');
+Route::middleware('auth')->group(
+    function () {
+        Route::get('/', function () {
+            return view('admin.index');
+        })->name('dashboard');
 
-});
+        Route::get('/dashboard', function () {
+            // return view('dashboard');
+            return view('admin.index');
+        })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+        Route::group(['namespace' => 'App\Modules\Profile\Controllers'], function () {
+            // Route::get('/', 'UserController@index');
+            // Route::get('/profile', 'UserController@profile');
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        });
+    }
+);
+
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        Route::middleware('role:admin')->group(function () {
-            Route:: get('/admin', function()   {
-                echo 1;
-            })->name('admin');
 
-        });
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/admin', function () {
+            echo 1;
+        })->name('admin');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
