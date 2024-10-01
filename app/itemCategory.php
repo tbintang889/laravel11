e8db9377c4f9a008c85904208d73624950388ad2
tbintang889 <115480259+tbintang889@users.noreply.github.com>
2024-10-01 18:13:58 +0700

    enum itemCategory

--- app/itemCategory.php
+++ app/itemCategory.php
@@ -4,8 +4,22 @@ namespace App;
 
 enum itemCategory: string
 {
-    case Pending = 'pending';
-    case Active = 'active';
-    case Inactive = 'inactive';
-    case Rejected = 'rejected';
+    case Raw_Goods = 'Raw Goods';
+    case Finish_Goods = 'Finish Goods';
+    case WIP = 'WIP';
+
+
+
+    // public function color(): string
+    // {
+    //     return match ($this) {
+    //         self::High => 'badge-danger',
+    //         self::Medium => 'badge-warning',
+    //         self::Low => 'badge-success',
+    //     };
+    // }
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value', 'name');
+    }
 }
